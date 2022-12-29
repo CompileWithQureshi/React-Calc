5dbad2c9fc23e2897b9cacd17c7e760734f3aef5
contactme.qureshi@gmail.com <contactme.qureshi@gmail.com>
2022-12-29 02:16:40 -0800

    Calculator compeleted

--- src/App.js
+++ src/App.js
@@ -1,29 +1,42 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [result, setResult] = useState('')
+
+  const handelClick = e => {
+    setResult(result + e.target.name)
+  }
+  const handelClear = e => {
+    setResult('')
+  }
+  const handeltotal = e => {
+    setResult(eval(result))
+  }
   return (
     <div className='App'>
       <div className='calculator'>
-        <div className='display'></div>
+
+        <div className='display' onChange={() => setResult()}>{result}</div>
         <div className='container'>
-          <button>C</button>
-          <button>/</button>
-          <button>%</button>
-          <button>*</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>-</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>+</button>
-          <button>3</button>
-          <button>2</button>
-          <button>1</button>
-          <button>.</button>
-          <button>0</button>
-          <button>=</button>
+          <button name='C' onClick={handelClear} className='bg-btn'>C</button>
+          <button name='/' onClick={handelClick} >/</button>
+          <button name='%' onClick={handelClick} className='bg-btn'>%</button>
+          <button name='*' onClick={handelClick} className='bg-btn'>x</button>
+          <button name='7' onClick={handelClick} >7</button>
+          <button name='8' onClick={handelClick} >8</button>
+          <button name='9' onClick={handelClick} >9</button>
+          <button name='-' onClick={handelClick} >-</button>
+          <button name='4' onClick={handelClick} >4</button>
+          <button name='5' onClick={handelClick} >5</button>
+          <button name='6' onClick={handelClick} >6</button>
+          <button name='+' onClick={handelClick} >+</button>
+          <button name='3' onClick={handelClick} >3</button>
+          <button name='2' onClick={handelClick} >2</button>
+          <button name='1' onClick={handelClick} >1</button>
+          <button name='.' onClick={handelClick} >.</button>
+          <button name='0' onClick={handelClick} >0</button>
+          <button className='bg-btn' name='=' onClick={handeltotal} >=</button>
 
 
         </div>
